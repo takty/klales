029fb0acfba389e66d69f2f5b8d6616b458fabad
Takuto Yanagida <yanagida@stxst.com>
2024-02-14 23:51:48 +0900

    Introduce stylelint

---
+++ .stylelintrc.mjs
@@ -0,0 +1,13 @@
+/** @type {import('stylelint').Config} */
+export default {
+	extends: [
+		'stylelint-config-standard'
+	],
+	rules: {
+		'at-rule-empty-line-before': null,
+		'custom-property-empty-line-before': null,
+		'declaration-empty-line-before': null,
+		'rule-empty-line-before': null,
+		'comment-whitespace-inside': null,
+	}
+};
